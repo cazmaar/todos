2b3d86cf1ee5c22146c450345526e8535b9e94be
cazmaar <kazeem.alatishe@yahoo.com>
2022-01-24 22:53:15 +0000

    Fetch and Post request done

--- server1/models/todos.js
+++ server1/models/todos.js
@@ -3,6 +3,7 @@ import query from "../db/index.js";
 // create a new todo
 export async function createTodo(body) {
   const { priority, tasks } = body;
+  console.log(priority);
   const res = await query(
     "INSERT INTO todos(priority, tasks) VALUES($1,$2) RETURNING *",
     [priority, tasks]
